126eeb79642bc41e119bdc7dcf1dd3e6c30ea014
“s6204062630491” <s6204062630491@email.kmutnb.ac.th>
2022-04-16 04:13:14 +0700

    second1

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,19 @@ import { StudentModule } from './students/students.module';
 import { StudentService } from './students/students.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), StudentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'student',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    StudentModule,
+  ],
   controllers: [AppController, StudentController],
   providers: [AppService, StudentService],
 })
